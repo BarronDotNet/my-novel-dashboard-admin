289efb078cb07638f4c020d9e1e9853ff370c2a1
BarronDotNet <supakit@mynovel.co>
2024-11-09 12:12:57 +0700

    feat: Suggestions for novel tag

--- src/components/dashboard/dashboard-books/edit-book/book-categories/novel-episode.tsx
+++ src/components/dashboard/dashboard-books/edit-book/book-categories/novel-episode.tsx
@@ -1,4 +1,5 @@
-import { Card } from '@/components/ui/card';
+'use client';
+import { Card, CardContent } from '@/components/ui/card';
 import UploadBookCover from '@/components/dashboard/dashboard-books/edit-book/upload-book-cover';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
@@ -6,7 +7,7 @@ import EditBookOptions from '@/components/dashboard/dashboard-books/edit-book/ed
 import { Textarea } from '@/components/ui/textarea';
 import EditBookTextEditorContent from '@/components/dashboard/dashboard-books/edit-book/edit-book-text-editor-content';
 import { IProductNovels } from '@/interfaces/product-novels.interface';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Select,
   SelectContent,
@@ -17,6 +18,11 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { Checkbox } from '@/components/ui/checkbox';
+import { Button } from '@/components/ui/button';
+import { CiSaveDown2 } from 'react-icons/ci';
+import { MdOutlineCancelPresentation } from 'react-icons/md';
+import { useRouter } from 'next/navigation';
+import { Badge } from '@/components/ui/badge';
 
 interface TagOption {
   value: string;
@@ -36,10 +42,10 @@ const novelTypeOptions: TagOption[] = [
 
 interface IProps {
   book?: IProductNovels | null;
-  loading: boolean;
 }
 
-const NovelEpisode = ({ book, loading }: IProps) => {
+const NovelEpisode = ({ book }: IProps) => {
+  const router = useRouter();
   const [mainCategory, setMainCategory] = useState('');
   const [secondaryCategory, setSecondaryCategory] = useState('');
   const [rate, setRate] = useState('');
@@ -50,6 +56,9 @@ const NovelEpisode = ({ book, loading }: IProps) => {
   const [fanClubTranslate, setFanClubTranslate] = useState('');
   const [productDetail, setProductDetail] = useState('');
   const [isPublish, setIsPublish] = useState(false);
+  const [productTags, setProductTags] = useState<string[]>([]);
+  const [newTag, setNewTag] = useState('');
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
     if (book) {
@@ -60,6 +69,7 @@ const NovelEpisode = ({ book, loading }: IProps) => {
       setProductIntro(book.ProductIntro || '');
       setProductDetail(book.ProductDetail || '');
       setFanClubTranslate(book.fanClubTranslate || '');
+      setProductTags(book.ProductTags || []);
       setIsPublish(book.isPublish || false);
       setProductAuthor(book.ProductAuthor || 'ไม่มีชื่อผู้แต่ง');
       setTranslator(book.Translator || 'ไม่มีชื่อผู้แปล');
@@ -68,6 +78,27 @@ const NovelEpisode = ({ book, loading }: IProps) => {
 
   const handlePublishToggle = () => {
     setIsPublish((prev) => !prev);
+  };
+
+  const handleCancelButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    router.push('/dashboard/dashboard-books/');
+  };
+
+  const handleAddTag = () => {
+    if (newTag && !productTags.includes(newTag)) {
+      setProductTags([...productTags, newTag]);
+      setNewTag('');
+    }
+    setShowSuggestions(false);
+  };
+
+  const handleSuggestionClick = (tag: string) => {
+    if (!productTags.includes(tag)) {
+      setProductTags([...productTags, tag]);
+    }
+    setNewTag('');
+    setShowSuggestions(false);
   };
 
   return (
@@ -134,6 +165,83 @@ const NovelEpisode = ({ book, loading }: IProps) => {
           </div>
 
           <div className="w-full grid gap-4 mb-3">
+            <Label htmlFor="addTag">แท็กนิยาย</Label>
+            <div className="relative">
+              <div className="flex gap-2 items-center">
+                <Input
+                  type="text"
+                  placeholder="เพิ่มแท็กของคุณ"
+                  value={newTag}
+                  onChange={(e) => {
+                    setNewTag(e.target.value);
+                    setShowSuggestions(true);
+                  }}
+                  onFocus={() => setShowSuggestions(true)}
+                />
+                <Button variant="outline" onClick={handleAddTag}>
+                  เพิ่มแท็ก
+                </Button>
+              </div>
+
+              {showSuggestions && (
+                <Card className="absolute left-0 right-0 mt-1 z-10 shadow-lg">
+                  <CardContent className="p-2 relative">
+                    <button
+                      onClick={() => setShowSuggestions(false)}
+                      className="absolute top-1 right-3 text-red-500 hover:text-red-700"
+                    >
+                      &times;
+                    </button>
+
+                    {initialTagsOption
+                      .filter((option) => option.label.includes(newTag))
+                      .map((option) => (
+                        <div
+                          key={option.value}
+                          onClick={() => handleSuggestionClick(option.value)}
+                          className="p-1 hover:bg-blue-100 cursor-pointer"
+                        >
+                          {option.label}
+                        </div>
+                      ))}
+
+                    {initialTagsOption.filter((option) =>
+                      option.label.includes(newTag)
+                    ).length === 0 &&
+                      newTag && (
+                        <div
+                          onClick={() => handleSuggestionClick(newTag)}
+                          className="p-1 hover:bg-blue-100 cursor-pointer text-gray-500 italic"
+                        >
+                          เพิ่มแท็ก &#34;{newTag}&#34;
+                        </div>
+                      )}
+                  </CardContent>
+                </Card>
+              )}
+            </div>
+
+            <div className="flex flex-wrap gap-2 mt-2">
+              {productTags.map((tag) => (
+                <Badge
+                  key={tag}
+                  className="flex items-center space-x-2 bg-blue-100 text-blue-700 px-2 py-1 rounded hover:bg-blue-200"
+                >
+                  <span>{tag}</span>
+                  <button
+                    onClick={() =>
+                      setProductTags(productTags.filter((t) => t !== tag))
+                    }
+                    className="ml-2 text-red-500 hover:text-red-700"
+                  >
+                    &times;
+                  </button>
+                </Badge>
+              ))}
+            </div>
+          </div>
+
+          <div className="w-full grid gap-4 mb-3">
             <div>
               <Label htmlFor="novelType">ประเภทนิยาย</Label>
               <Select
@@ -181,6 +289,18 @@ const NovelEpisode = ({ book, loading }: IProps) => {
           content={productDetail}
           onContentChange={setProductDetail}
         />
+      </div>
+
+      <div className="flex justify-start items-center gap-3 mt-3">
+        <Button
+          variant="outline"
+          className="border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white"
+        >
+          <CiSaveDown2 /> บันทึก
+        </Button>
+        <Button variant="outline" onClick={handleCancelButton}>
+          <MdOutlineCancelPresentation /> ยกเลิก
+        </Button>
       </div>
     </div>
   );
